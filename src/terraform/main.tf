1c35e6c2b886e4390678d4388b3801a190bcf878
Dan Kitchen <140333496+dankitch@users.noreply.github.com>
2024-04-09 14:24:28 +0100

    Add Azure naming module and avm-res-network-virtualnetwork module

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -8,3 +8,27 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-${var.application_name}-${var.environment_name}-${random_string.main.result}"
   location = var.primary_region
 }
+
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+locals {
+  subnets = {
+    for i in range(3) : "subnet${i}" => {
+      address_prefixes = [cidrsubnet(local.virtual_network_address_space, 8, i)]
+    }
+  }
+  virtual_network_address_space = "10.50.24.0/16"
+}
+
+module "avm-res-network-virtualnetwork" {
+  source                        = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version                       = "0.1.4"
+  resource_group_name           = azurerm_resource_group.main.name
+  location                      = var.primary_region
+  name                          = module.naming.virtual_network.name
+  subnets                       = local.subnets
+  virtual_network_address_space = local.virtual_network_address_space
+}
