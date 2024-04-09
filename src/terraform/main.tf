c83af17d2eb4cc030b5d21c19306e69e120c9816
Dan Kitchen <140333496+dankitch@users.noreply.github.com>
2024-04-09 14:55:36 +0100

    Disable telemetry in avm-res-network-virtualnetwork module

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -30,8 +30,10 @@ module "avm-res-network-virtualnetwork" {
   location                      = var.primary_region
   name                          = module.naming.virtual_network.name
   subnets                       = local.subnets
+  enable_telemetry              = false
   virtual_network_address_space = ["10.0.0.0/16"]
   virtual_network_dns_servers = {
     dns_servers = ["8.8.8.8"]
   }
+
 }
