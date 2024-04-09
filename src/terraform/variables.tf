3364a968099d5ce69c15983c405ae6b71319c6b0
Dan Kitchen <140333496+dankitch@users.noreply.github.com>
2024-04-09 13:43:35 +0100

    Update primary_region variable description

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -5,5 +5,6 @@ variable "environment_name" {
   type = string
 }
 variable "primary_region" {
-  type = string
+  type        = string
+  description = "The region where the resources will be created"
 }
